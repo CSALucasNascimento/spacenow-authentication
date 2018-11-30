07eec116b125e4d0f55664897c5ec327e5d97a39
Lucas Nascimento <csalucasnascimento@gmail.com>
2018-11-30 20:26:26 +1100

    auth component

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from "react-dom"
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Authentication from './Authentication'
+
+ReactDOM.render(<Authentication />, document.getElementById('root'))
